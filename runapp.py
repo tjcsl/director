6e8fd09962e177e49a8879b8471c146feb50c5b0
Fox Wilson <2016fwilson@amdahl.csl.tjhsst.edu>
2016-02-19 10:50:28 -0500

    debug -> true

--- runapp.py
+++ runapp.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "debug":
         app.run(port=4998, debug=True, host='0.0.0.0')
     else:
-        app.run(port=4998, debug=False, host='127.0.0.1')
+        app.run(port=4998, debug=True, host='127.0.0.1')
