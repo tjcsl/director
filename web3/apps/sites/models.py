d7c55fbcbf59cd8f808c699d0aae056c33f111fe
Eric Wang <ezwang1@gmail.com>
2016-11-05 19:21:22 -0400

    create website model

--- web3/apps/sites/models.py
+++ web3/apps/sites/models.py
@@ -1,5 +1,35 @@
 from __future__ import unicode_literals
 
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+from ..users.models import User, Group
+
+
+class Website(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, validators=[validators.MinValueValidator(1000)])
+    name = models.CharField(max_length=32, unique=True)
+    category = models.CharField(max_length=16, choices=(
+        ("legacy", "legacy"),
+        ("static", "static"),
+        ("php", "php"),
+        ("dynamic", "dynamic")
+    ))
+    purpose = models.CharField(max_length=16, choices=(
+        ("user", "user"),
+        ("activity", "activity")
+    ))
+    domain = models.TextField()
+    description = models.TextField()
+
+    user = models.ForeignKey(User)
+    group = models.ForeignKey(Group)
+
+    @property
+    def path(self):
+        if self.purpose == "user":
+            return "/web/user/{}/".format(self.name)
+        elif self.purpose == "activity":
+            return "/web/activities/{}/".format(self.name)
+        else:
+            return "/web/{}/".format(self.name)
