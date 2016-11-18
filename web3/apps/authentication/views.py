49e04cac671031efd1dcdc8ef05574c514a6f8ad
Eric Wang <ezwang1@gmail.com>
2016-11-17 23:37:05 -0500

    list sites on dashboard

--- web3/apps/authentication/views.py
+++ web3/apps/authentication/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
+from ..sites.models import Site
 
 def index_view(request):
     if request.user.is_authenticated():
-        return render(request, "index.html", {})
+        return render(request, "dashboard.html", {
+            "sites": Site.objects.filter(group__users__id=request.user.id).order_by("name")
+        })
     else:
         return login_view(request)
 
