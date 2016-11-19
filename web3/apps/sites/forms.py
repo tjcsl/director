f4b313df3d8b057ba6402c5cf5c6f31aca6cd5a1
Eric Wang <ezwang1@gmail.com>
2016-11-18 19:24:35 -0500

    make web form more friendly

--- web3/apps/sites/forms.py
+++ web3/apps/sites/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class CreateSiteForm(forms.Form):
     name = forms.CharField(max_length=32, widget=forms.TextInput(attrs={"class": "form-control"}))
-    domain = forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}))
+    domain = forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"}), help_text="Can only contain A-Z, a-z, 0-9 and the - character. Separate multiple domains through spaces.")
     description = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     category = forms.ChoiceField(choices=(("static", "Static"), ("php", "PHP"), ("dynamic", "Dynamic")), widget=forms.Select(attrs={"class": "form-control"}))
     purpose = forms.ChoiceField(choices=(("user", "User"), ("activity", "Activity")), widget=forms.Select(attrs={"class": "form-control"}))
---
+++ web3/static/js/sites/creation.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#id_name").on("paste keyup blur", function(e) {
+        var new_name = $(this).val();
+        var current_domain = $("#id_domain").val();
+        if (!current_domain || current_domain.includes(".sites.tjhsst.edu")) {
+            if (new_name) {
+                $("#id_domain").val(new_name.replace(" ", "-") + ".sites.tjhsst.edu");
+            }
+            else {
+                $("#id_domain").val("");
+            }
+        }
+    });
+});
