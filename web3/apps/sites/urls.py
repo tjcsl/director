2284265e0a1b96a8d2536f5210eb40b220c462d0
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-11-19 16:15:32 -0500

    fix urls

--- web3/apps/sites/urls.py
+++ web3/apps/sites/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url("^edit/(?P<site_id>\d+)$", views.edit_view, name="edit_site"),
     url("^delete/(?P<site_id>\d+)$", views.delete_view, name="delete_site"),
     url("^action/permission/(?P<site_id>\d+)$", views.permission_view, name="permission_site"),
-    url("^action/config/(?P<site_id>\d+)$", views.config_view, name="config_site")
-    url("^all$", views.all_sites_view, name="all_sites")
+    url("^action/config/(?P<site_id>\d+)$", views.config_view, name="config_site"),
+    url("^all$", views.all_sites_view, name="all_sites"),
 ]
