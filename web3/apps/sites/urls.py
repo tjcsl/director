79de109c6f2b49c57f95e26f85ad9e9f127fd698
Eric Wang <ezwang1@gmail.com>
2016-11-19 16:04:47 -0500

    add config/permission regen function

--- web3/apps/sites/urls.py
+++ web3/apps/sites/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url("^create$", views.create_view, name="create_site"),
     url("^info/(?P<site_id>\d+)$", views.info_view, name="info_site"),
     url("^edit/(?P<site_id>\d+)$", views.edit_view, name="edit_site"),
-    url("^delete/(?P<site_id>\d+)$", views.delete_view, name="delete_site")
+    url("^delete/(?P<site_id>\d+)$", views.delete_view, name="delete_site"),
+    url("^action/permission/(?P<site_id>\d+)$", views.permission_view, name="permission_site"),
+    url("^action/config/(?P<site_id>\d+)$", views.config_view, name="config_site")
 ]
--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Site
 from .forms import SiteForm
-from .helpers import reload_services, delete_site_files
+from .helpers import reload_services, delete_site_files, create_config_files, make_site_dirs
 
 from ..authentication.decorators import superuser_required
 
@@ -67,6 +67,36 @@ def delete_view(request, site_id):
         "site": site
     }
     return render(request, "sites/delete_site.html", context)
+
+
+@login_required
+def config_view(request, site_id):
+    site = get_object_or_404(Site, id=site_id)
+    if not request.user.is_superuser and not site.group.users.filter(id=request.user.id).exists():
+        raise PermissionDenied
+
+    if not settings.DEBUG:
+        create_config_files(site)
+        reload_services()
+
+    messages.success(request, "Configuration files regenerated!")
+    return redirect("info_site", site_id=site_id)
+
+
+@login_required
+def permission_view(request, site_id):
+    site = get_object_or_404(Site, id=site_id)
+    if not request.user.is_superuser and not site.group.users.filter(id=request.user.id).exists():
+        raise PermissionDenied
+
+    if not settings.DEBUG:
+        make_site_dirs(site)
+        for root, dirs, files in os.walk(site.path):
+            for f in files + dirs:
+                os.chown(os.path.join(root, f), site.user.id, site.group.id)
+
+    messages.success(request, "File permissions regenerated!")
+    return redirect("info_site", site_id=site_id)
 
 
 @login_required
