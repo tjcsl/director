43570f20ae25189ad518e78246d502cb9e56a3bd
Eric Wang <ezwang1@gmail.com>
2016-11-18 20:17:08 -0500

    add site page

--- web3/apps/sites/urls.py
+++ web3/apps/sites/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("^create$", views.create_view, name="create_site")
+    url("^create$", views.create_view, name="create_site"),
+    url("^info/(?P<site_id>\d+)$", views.info_view, name="info_site")
 ]
--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
@@ -27,4 +27,10 @@ def create_view(request):
     }
     return render(request, "sites/create_site.html", context)
 
-
+@login_required
+def info_view(request, site_id):
+    site = get_object_or_404(Site, id=site_id)
+    context = {
+        "site": site
+    }
+    return render(request, "sites/info_site.html", context)
