660c21181c8f99a9b63190eec1b8c7470192c129
Eric Wang <ezwang1@gmail.com>
2016-11-20 19:13:02 -0500

    add user edit

--- web3/apps/users/urls.py
+++ web3/apps/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url("^settings$", views.settings_view, name="user_settings"),
-    url("^create$", views.create_user_view, name="create_user"),
-    url("^management$", views.manage_user_view, name="user_management")
+    url("^create$", views.create_view, name="create_user"),
+    url("^edit/(?P<user_id>\d+)$", views.edit_view, name="edit_user"),
+    url("^management$", views.manage_view, name="user_management")
 ]
--- web3/apps/users/views.py
+++ web3/apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -16,7 +16,7 @@ def settings_view(request):
 
 
 @superuser_required
-def create_user_view(request):
+def create_view(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -31,8 +31,27 @@ def create_user_view(request):
     }
     return render(request, "users/create_user.html", context)
 
+
 @superuser_required
-def manage_user_view(request):
+def edit_view(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            messages.success(request, "User {} edited!".format(user.username))
+            return redirect("user_management")
+    else:
+        form = UserForm(instance=user)
+
+    context = {
+        "form": form
+    }
+    return render(request, "users/create_user.html", context)
+
+
+@superuser_required
+def manage_view(request):
     context = {
         "users": User.objects.filter(service=False).order_by("username")
     }
