720a0449a3af7002520cc628b20444ac7d0fb68f
Eric Wang <ezwang1@gmail.com>
2016-11-23 18:20:48 -0500

    tag for no users

--- web3/apps/authentication/views.py
+++ web3/apps/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.db.models import Count
 
 from ..sites.models import Site
 
@@ -7,8 +8,8 @@ from ..sites.models import Site
 def index_view(request):
     if request.user.is_authenticated():
         return render(request, "dashboard.html", {
-            "sites": Site.objects.filter(group__users__id=request.user.id).order_by("name"),
-            "other_sites": Site.objects.exclude(group__users__id=request.user.id).order_by("name") if request.user.is_superuser else None
+            "sites": Site.objects.filter(group__users__id=request.user.id).annotate(num_users=Count("group__users")).order_by("name"),
+            "other_sites": Site.objects.exclude(group__users__id=request.user.id).annotate(num_users=Count("group__users")).order_by("name") if request.user.is_superuser else None
         })
     else:
         return login_view(request)
