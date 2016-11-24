8a21f6d045801e6d5bc9ef08e2e13b46c3c7ab5c
Eric Wang <ezwang1@gmail.com>
2016-11-24 09:20:02 -0500

    show error

--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -229,6 +229,8 @@ def do_git_pull(site):
 def get_latest_commit(site):
     if not settings.DEBUG:
         output = run_as_site(site, ["git", "show", "-s", "-format=%h %s", "HEAD"], cwd=site.public_path)
+        if not output[0] == 0:
+            return "Error - {}".format(output[2].replace("\n", " ").replace("\r", ""))
         return output[1]
     else:
         return "abacada example commit"
