439c45145074bdc054f4d7e2c0ef79d9bbf681da
Eric Wang <ezwang1@gmail.com>
2016-11-26 16:25:11 -0500

    implement console history

--- web3/static/js/sql_console.js
+++ web3/static/js/sql_console.js
@@ -7,6 +7,31 @@ $(document).ready(function() {
             xhr.setRequestHeader("X-CSRFToken", csrf_token);
         }
     });
+    var history = [];
+    var history_point = 0;
+    $(document).keydown(function(e) {
+        if (e.which == 38) {
+            history_point += 1;
+            if (history_point < history.length) {
+                $("#input").val(history[history_point]);
+            }
+            else {
+                history_point = history.length - 1;
+            }
+            e.preventDefault();
+        }
+        if (e.which == 40) {
+            history_point -= 1;
+            if (history_point >= 0 && history.length > 0) {
+                $("#input").val(history[history_point]);
+            }
+            else {
+                $("#input").val("");
+                history_point = -1;
+            }
+            e.preventDefault();
+        }
+    });
     $("#input").keypress(function(e) {
         if (e.which == 13) {
             var val = $(this).val();
@@ -24,6 +49,8 @@ $(document).ready(function() {
                     $("#input").focus();
                     $("#console").scrollTop($("#console")[0].scrollHeight);
                 });
+                history.unshift(val);
+                history_point = -1;
                 $(this).val("");
             }
             else {
