10967e7f8346aba187fde00df26b19eba8165cfe
Eric Wang <ezwang1@gmail.com>
2016-11-27 13:28:57 -0500

    error handling

--- web3/apps/sites/helpers.py
+++ web3/apps/sites/helpers.py
@@ -271,14 +271,14 @@ def create_mysql_database(database):
     conn = MySQLdb.connect(host=settings.MYSQL_DB_HOST, user=settings.MYSQL_DB_USER, password=settings.MYSQL_DB_PASS)
     cursor = conn.cursor()
     try:
-        cursor.execute("SELECT 1 FROM mysql.user WHERE user = ''".format(database.username))
+        cursor.execute("SELECT 1 FROM mysql.user WHERE user = '{}'".format(database.username))
         if cursor.rowcount == 0:
             cursor.execute("CREATE USER '{}'@'%' IDENTIFIED BY '{}';".format(database.username, database.password))
         cursor.execute("CREATE DATABASE IF NOT EXISTS {};".format(database.db_name))
         cursor.execute("GRANT ALL ON {} . * TO {};".format(database.db_name, database.username))
         cursor.execute("FLUSH PRIVILEGES;")
         return True
-    except MySQLProgrammingError:
+    except (MySQLProgrammingError, MySQLOperationalError):
         client.captureException()
         return False
     finally:
