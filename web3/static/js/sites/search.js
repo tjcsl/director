19a2bd479e2f77474e63991c410df0290ff40a60
Eric Wang <2017ewang@tjhsst.edu>
2016-11-28 12:05:28 -0500

    more search improvements

--- web3/static/js/sites/search.js
+++ web3/static/js/sites/search.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     $("#select-site").on("input", function(){
         var val = $(this).val().toLowerCase();
+
+        var notags = [];
+        var nomatches = val.match(/notag:\w+/g);
+        if (nomatches) {
+            $.each(nomatches, function(k, v) {
+                notags.push(v.substring(6).toLowerCase());
+            });
+        }
+        val = $.trim(val.replace(/\s*notag:\w+/g, ""));
+
         var tags = [];
         var matches = val.match(/tag:\w+/g);
         if (matches) {
@@ -14,16 +24,29 @@ $(document).ready(function() {
             var desc = $(this).find(".desc").text();
             var type = $(this).find(".type").text();
             var show = true;
-            if (tags.length) {
-                var site_tags = [];
+            var site_tags = null;
+            if (tags.length || notags.length) {
+                site_tags = [];
                 $(this).find(".tag").each(function(k, v) {
                     site_tags.push($(this).text().replace(/\W/g, "").toLowerCase());
                 });
+            }
+            if (tags.length) {
                 show = false;
                 $.each(tags, function(k, v) {
                     $.each(site_tags, function(k2, v2) {
-                        if (v2.indexOf(v) == 0) {
+                        if (v2.indexOf(v) != -1) {
                             show = true;
+                            return false;
+                        }
+                    });
+                });
+            }
+            if (notags.length) {
+                $.each(notags, function(k, v) {
+                    $.each(site_tags, function(k2, v2) {
+                        if (v2.indexOf(v) != -1) {
+                            show = false;
                             return false;
                         }
                     });
