baafadfb01899d1beb6725a63e8d943e6fdde8b2
Eric Wang <ezwang1@gmail.com>
2016-12-03 11:54:05 -0500

    add command to migrate vms

---
+++ web3/apps/vms/management/commands/migrate_vms.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ...models import VirtualMachine
+from ...helpers import call_api
+
+
+class Command(BaseCommand):
+    help = "Migrates VMs over from the old Conductor system"
+
+    def handle(self, *args, **options):
+        containers = call_api("container.list")
+        for container in containers:
+            if VirtualMachine.objects.filter(uuid=container).exists():
+                pass
+            else:
+                print("[*] Importing {}...".format(container))
+                VirtualMachine.objects.create(name="Unknown VM {}".format(container), uuid=uuid.UUID(container), description="Unknown Virtual Machine")
