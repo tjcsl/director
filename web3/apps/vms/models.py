e2203e4e9bac0c0f3002c7a7925c72a17a46426f
Eric Wang <ezwang1@gmail.com>
2016-12-03 12:57:41 -0500

    make name limit 255

--- web3/apps/vms/models.py
+++ web3/apps/vms/models.py
@@ -6,7 +6,7 @@ from ..users.models import User
 
 
 class VirtualMachine(models.Model):
-    name = models.CharField(max_length=36, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     description = models.TextField(blank=True)
     users = models.ManyToManyField(User, related_name='vms')
