28099d6e2a26446e09b55855a9f95305979b3076
Eric Wang <ezwang1@gmail.com>
2016-12-03 10:25:14 -0500

    add uuid field for conductor

--- web3/apps/vms/models.py
+++ web3/apps/vms/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 from ..users.models import User
@@ -5,6 +7,6 @@ from ..users.models import User
 
 class VirtualMachine(models.Model):
     name = models.CharField(max_length=36, unique=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     description = models.TextField(blank=True)
-
     users = models.ManyToManyField(User, related_name='vms')
