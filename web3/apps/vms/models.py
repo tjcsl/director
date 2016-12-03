115af68533f9a0ed714b9230ec4c39a9686b004b
Eric Wang <ezwang1@gmail.com>
2016-12-02 19:21:07 -0500

    add vm model

--- web3/apps/vms/models.py
+++ web3/apps/vms/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from ..users.models import User
+
+
+class VirtualMachine(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.TextField(blank=True)
+
+    users = models.ManyToManyField(User, related_name='vm_groups')
