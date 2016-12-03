4b8f2df0ac583cc819c37f57aeff8db7a1bd8f69
Eric Wang <ezwang1@gmail.com>
2016-12-02 20:56:36 -0500

    implement deletion

--- web3/apps/vms/urls.py
+++ web3/apps/vms/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url("^list$", views.list_view, name="vm_list"),
     url("^create$", views.create_view, name="create_vm"),
-    url("^(?P<vm_id>\d+)$", views.info_view, name="vm_info")
+    url("^(?P<vm_id>\d+)$", views.info_view, name="vm_info"),
+    url("^(?P<vm_id>\d+)/delete$", views.delete_view, name="vm_delete")
 ]
--- web3/apps/vms/views.py
+++ web3/apps/vms/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from ..authentication.decorators import superuser_required
+from django.core.exceptions import PermissionDenied
 
 from .forms import VirtualMachineForm
 from .models import VirtualMachine
@@ -15,7 +16,27 @@ def list_view(request):
 @login_required
 def info_view(request, vm_id):
     vm = get_object_or_404(VirtualMachine, id=vm_id)
+    if not request.user.is_superuser and not vm.users.filter(id=request.user.id).exists():
+        raise PermissionDenied
+
     return render(request, "vms/info.html", {"vm": vm})
+
+
+@login_required
+def delete_view(request, vm_id):
+    vm = get_object_or_404(VirtualMachine, id=vm_id)
+    if not request.user.is_superuser and not vm.users.filter(id=request.user.id).exists():
+        raise PermissionDenied
+
+    if request.method == "POST":
+        if request.POST.get("confirm", None) == vm.name:
+            vm.delete()
+            messages.success(request, "Virtual machine deleted!")
+            return redirect("vm_list")
+        else:
+            messages.error(request, "Failed deletion confirmation!")
+
+    return render(request, "vms/delete.html", {"vm": vm})
 
 
 @superuser_required
