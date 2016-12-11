053d59a50d0dd7af4c4c02b74cb1f624a6a707d4
Eric Wang <ezwang1@gmail.com>
2016-12-11 12:03:51 -0500

    catch 400 too

--- web3/apps/vms/helpers.py
+++ web3/apps/vms/helpers.py
@@ -9,7 +9,7 @@ from raven.contrib.django.raven_compat.models import client
 
 def call_api(action=None, **kwargs):
     resp = requests.request(method=("POST" if action else "GET"), json={"method": action, "args": kwargs}, url=settings.CONDUCTOR_AGENT_PATH, cert=settings.CONDUCTOR_CERT_PATH, verify=False)
-    if resp.status_code == 500:
+    if resp.status_code == 500 or resp.status_code == 400:
         if settings.DEBUG:
             print("{} {}\n{} {}".format(resp.status_code, resp.text, action, kwargs))
         else:
