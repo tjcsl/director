68d957e26978c5a09e69230a1e6f8ae2035da74b
Eric Wang <ezwang1@gmail.com>
2016-12-13 22:39:20 -0500

    fix typo

--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -547,7 +547,7 @@ def generate_key_view(request, site_id):
 
 
 @login_required
-def web_terminal_view(request, id=site_id):
+def web_terminal_view(request, site_id):
     site = get_object_or_404(Site, id=site_id)
     if not request.user.is_superuser and not site.group.users.filter(id=request.user.id).exists():
         raise PermissionDenied
