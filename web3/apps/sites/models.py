0dbb50369a547748865df42b85632bf06a2e53a8
Eric Wang <ezwang1@gmail.com>
2016-12-13 23:20:17 -0500

    fix db issue

--- web3/apps/sites/models.py
+++ web3/apps/sites/models.py
@@ -119,13 +119,13 @@ class Database(models.Model):
 
     @property
     def db_name(self):
-        return "site_{}".format(self.site.name)
+        return "site_{}".format(self.site.name).lower()
 
     @property
     def username(self):
         if self.category == "mysql":
             return "site_{}".format(self.site.name)[:16]
-        return "site_{}".format(self.site.name)
+        return "site_{}".format(self.site.name).lower()
 
     def __str__(self):
         return "{}://{}:{}@{}/{}".format("postgres" if self.category == "postgresql" else "mysql", self.username, self.password, "postgres1:5432" if self.category == "postgresql" else "mysql1:3306", self.db_name)
