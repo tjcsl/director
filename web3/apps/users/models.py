6ccbdfb2ef16cfe82156a26ba2ffdd659f6122e0
Eric Wang <ezwang1@gmail.com>
2016-12-14 19:12:09 -0500

    fix access_token unset

--- web3/apps/users/models.py
+++ web3/apps/users/models.py
@@ -24,7 +24,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
     github_token = models.CharField(max_length=40, blank=True)
-    access_token = models.CharField(max_length=64, blank=True)
+    access_token = models.CharField(max_length=64, blank=True, null=True)
     USERNAME_FIELD = 'username'
 
     objects = UserManager()
