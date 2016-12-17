2c78b3eac4ac3c2681d621a2c6d3b0feaf07a8af
Eric Wang <ezwang1@gmail.com>
2016-12-17 12:36:40 -0500

    fix typo

--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -647,7 +647,7 @@ def git_setup_view(request, site_id):
 
 
 @login_required
-def editor(request, site_id):
+def editor_view(request, site_id):
     site = get_object_or_404(Site, id=site_id)
     if not request.user.is_superuser and not site.group.users.filter(id=request.user.id).exists():
         raise PermissionDenied
