383af951c1a6b8482af79884a7275323c71cf0e6
Eric Wang <ezwang1@gmail.com>
2016-12-17 14:17:57 -0500

    method name

--- web3/apps/sites/views.py
+++ web3/apps/sites/views.py
@@ -706,7 +706,7 @@ def editor_load_view(request, site_id):
 
 
 @login_required
-def editor_load_view(request, site_id):
+def editor_save_view(request, site_id):
     site = get_object_or_404(Site, id=site_id)
     if not request.user.is_superuser and not site.group.users.filter(id=request.user.id).exists():
         raise PermissionDenied
