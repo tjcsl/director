129a6ce1fa64be3470f660a62174be57ba8875dc
Eric Wang <ezwang1@gmail.com>
2016-12-17 23:23:39 -0500

    bit more offset

--- web3/static/js/sites/editor.js
+++ web3/static/js/sites/editor.js
@@ -399,7 +399,7 @@ You can drag files and folders around to move them.");
         sep_dragging = false;
     }).mousemove(function(e) {
         if (sep_dragging) {
-            var newPos = e.clientX - 8;
+            var newPos = e.clientX - 10;
             $("#files").width(newPos);
             $("#editor-wrapper").css("width", "calc(100vw - 15px - " + newPos + "px)");
         }
