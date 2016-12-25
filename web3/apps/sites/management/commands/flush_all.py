ad6e6d3f9dd4a3a9356df72e0dc40326509c4bca
Eric Wang <ezwang1@gmail.com>
2016-12-25 14:06:05 -0500

    add flush all command

---
+++ web3/apps/sites/management/commands/flush_all.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from web3.apps.sites.models import Site
+from web3.apps.sites.helpers import make_site_dirs, fix_permissions, reload_services, reload_nginx_config
+
+import shutil
+import os
+
+
+class Command(BaseCommand):
+    help = "Rewrite all site configurations and reset all permissions"
+
+    def handle(self, **options):
+        for site in Site.objects.all():
+            make_site_dirs(site)
+            fix_permissions(site)
+            create_config_files(site)
+        reload_services()
+        reload_nginx_config()
