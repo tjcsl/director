f91a6126868b1722af41dade4e369f4d215ff533
Eric Wang <ezwang1@gmail.com>
2017-01-15 17:54:34 -0500

    support ipv4

--- agent/views/updatedns.py
+++ agent/views/updatedns.py
@@ -1,23 +1,27 @@
 from agent import rpc
+from ipaddress import ip_address
 
 import dns.query
 import dns.tsigkeyring
 import dns.update
-import sys
 
 keyring = dns.tsigkeyring.from_text({
     'vm.sites.tjhsst.edu.': open("/root/dns.key", "r").read().strip()
 })
 
+
 @rpc.method("dns.add")
 def add_dns(host, ip):
     update = dns.update.Update('vm.sites.tjhsst.edu', keyring=keyring)
-    update.add(host, 86400, 'AAAA', ip)
-    response = dns.query.tcp(update, 'deneb.csl.tjhsst.edu')
+    if ip_address(ip).version == 6:
+        update.add(host, 86400, 'AAAA', ip)
+    else:
+        update.add(host, 86400, 'A', ip)
+    dns.query.tcp(update, 'deneb.csl.tjhsst.edu')
 
 
 @rpc.method("dns.remove")
 def remove_dns(host):
     update = dns.update.Update('vm.sites.tjhsst.edu', keyring=keyring)
     update.delete(host)
-    response = dns.query.tcp(update, 'deneb.csl.tjhsst.edu')
+    dns.query.tcp(update, 'deneb.csl.tjhsst.edu')
