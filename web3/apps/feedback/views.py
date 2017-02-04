1b1e96a968548c76d4dc5bd8f0d129760f1220c4
Eric Wang <ezwang1@gmail.com>
2017-02-04 16:12:08 -0500

    redirect on submit

--- web3/apps/feedback/views.py
+++ web3/apps/feedback/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def feedback_view(request):
+    if request.method == "POST":
+        comments = request.POST.get("feedback", None)
+        messages.success(request, "Your feedback has been received! Thanks!")
+        return redirect("index")
     return render(request, "feedback/feedback.html")
