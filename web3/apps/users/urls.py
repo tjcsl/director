b89bfed9edcaaa46fe2a0be57f43f08eb0b65a56
Eric Wang <ezwang1@gmail.com>
2017-02-09 22:04:58 -0500

    shorten urls

--- web3/apps/users/urls.py
+++ web3/apps/users/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url("^$", views.manage_view, name="user_management"),
     url("^settings$", views.settings_view, name="user_settings"),
     url("^github/link$", views.github_link_view, name="link_github"),
     url("^github/unlink$", views.github_unlink_view, name="unlink_github"),
     url("^create$", views.create_view, name="create_user"),
     url("^edit/(?P<user_id>\d+)$", views.edit_view, name="edit_user"),
-    url("^management$", views.manage_view, name="user_management"),
     url("^webdocs$", views.create_webdocs_view, name="create_webdocs"),
     url("^lookup$", views.teacher_lookup_view, name="teacher_lookup")
 ]
--- web3/apps/vms/urls.py
+++ web3/apps/vms/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("^list$", views.list_view, name="vm_list"),
+    url("^$", views.list_view, name="vm_list"),
     url("^create$", views.create_view, name="create_vm"),
     url("^(?P<vm_id>\d+)$", views.info_view, name="vm_info"),
     url("^(?P<vm_id>\d+)/edit$", views.edit_view, name="vm_edit"),
