add9bcd9204997a6e2741810d6ea1c0ca00a2ad5
Eric Wang <ezwang1@gmail.com>
2017-02-25 19:49:54 -0500

    sql console ids -> classes

--- web3/static/js/sql_console.js
+++ web3/static/js/sql_console.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $("#console").click(function() {
+    $(".sql-console").click(function() {
         var sel = window.getSelection().toString();
         if (!sel) {
-            $("#console .input").focus();
+            $(this).find(".input").focus();
         }
     });
     var history = [];
     var history_point = 0;
-    $(document).keydown(function(e) {
+    $(".sql-console").keydown(function(e) {
         if (e.which == 38) {
             history_point += 1;
             if (history_point < history.length) {
-                $("#console .input").val(history[history_point]);
+                $(this).find(".input").val(history[history_point]);
             }
             else {
                 history_point = history.length - 1;
@@ -21,27 +21,29 @@ $(document).ready(function() {
         if (e.which == 40) {
             history_point -= 1;
             if (history_point >= 0 && history.length > 0) {
-                $("#console .input").val(history[history_point]);
+                $(this).find(".input").val(history[history_point]);
             }
             else {
-                $("#console .input").val("");
+                $(this).find(".input").val("");
                 history_point = -1;
             }
             e.preventDefault();
         }
     });
     $.post(sql_endpoint, {"version": true}, function(data) {
-        $("#console .output").append(data + "\n");
+        console.find(".output").append(data + "\n");
     });
-    $("#console .input").keydown(function(e) {
+    console.find(".input").keydown(function(e) {
+        var console = $(this).parent();
         if (e.ctrlKey && e.keyCode == 67 && !window.getSelection().toString()) {
-            $("#console .output").append($("#console .ps").text() + $(this).val() + "^C\n");
+            console.find(".output").append(console.find(".ps").text() + $(this).val() + "^C\n");
             $(this).val("");
-            $("#console").scrollTop($("#console")[0].scrollHeight);
+            console.scrollTop(console[0].scrollHeight);
             e.preventDefault();
         }
     });
-    $("#console .input").keypress(function(e) {
+    console.find(".input").keypress(function(e) {
+        var console = $(this).parent();
         if (e.which == 13) {
             var val = $(this).val();
             if (val) {
@@ -52,19 +54,19 @@ $(document).ready(function() {
                     }
                 }
                 else if ($.trim(val.toLowerCase()) == "\\! clear") {
-                    $("#console .output").text("");
+                    console.find(".output").text("");
                 }
                 else {
-                    $("#console .output").append($("#console .ps").text() + val + "\n");
-                    $("#console table").hide();
+                    console.find(".output").append(console.find(".ps").text() + val + "\n");
+                    console.find("table").hide();
                     $.post(sql_endpoint, {"sql": val}, function(data) {
-                        $("#console .output").append($("<div />").text(data).html());
+                        console.find(".output").append($("<div />").text(data).html());
                     }).error(function() {
-                        $("#console .output").append("<span style='color:#cc0000'>Server Error</span>\n\n");
+                        console.find(".output").append("<span style='color:#cc0000'>Server Error</span>\n\n");
                     }).always(function() {
-                        $("#console table").show();
-                        $("#console .input").focus();
-                        $("#console").scrollTop($("#console")[0].scrollHeight);
+                        console.find("table").show();
+                        console.find(".input").focus();
+                        console.scrollTop(console[0].scrollHeight);
                     });
                     history.unshift(val);
                     history_point = -1;
@@ -72,9 +74,9 @@ $(document).ready(function() {
                 $(this).val("");
             }
             else {
-                $("#console .output").append($("#console .ps").text() + "\n");
+                console.find(".output").append(console.find(".ps").text() + "\n");
             }
-            $("#console").scrollTop($("#console")[0].scrollHeight);
+            console.scrollTop(console[0].scrollHeight);
         }
     });
 });
