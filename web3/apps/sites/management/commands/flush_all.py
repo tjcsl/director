eb516ca3ac88435ab93664594a59072e4c4be412
Eric Wang <2017ewang@tjhsst.edu>
2017-03-02 15:42:48 -0500

    add flag to management command

--- web3/apps/sites/management/commands/flush_all.py
+++ web3/apps/sites/management/commands/flush_all.py
@@ -11,13 +11,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--no-permissions', action='store_true', dest='noperms', default=False, help="Don't reset permissions.")
+        parser.add_argument('--no-config', action='store_true', dest='noconfig', default=False, help="Don't create configuration files.")
 
     def handle(self, **options):
         for site in Site.objects.all():
             make_site_dirs(site)
             if not options["noperms"]:
                 fix_permissions(site)
-            create_config_files(site)
+            if not options["noconfig"]:
+                create_config_files(site)
             self.stdout.write("Finished {}".format(site.name))
         reload_services()
         reload_nginx_config()
