8b93b0bb12f15a51bd66f0fa3267938f0703e15d
Eric Wang <ezwang1@gmail.com>
2017-03-14 15:30:46 -0400

    better feedback handling

--- web3/utils/emails.py
+++ web3/utils/emails.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives, send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 
@@ -33,7 +33,8 @@ def send_feedback_email(request, comments):
     context = {
         "request": request,
         "feedback": comments,
-        "useragent": request.META["HTTP_USER_AGENT"]
+        "useragent": request.META["HTTP_USER_AGENT"],
+        "remote_ip": get_remote_ip(request)
     }
     plain_message = render_to_string("emails/feedback.txt", context)
     html_message = render_to_string("emails/feedback.html", context)
@@ -41,6 +42,18 @@ def send_feedback_email(request, comments):
         print(plain_message)
         return 0
     else:
-        return send_mail("{} Director Feedback".format(settings.EMAIL_SUBJECT_PREFIX),
-                        plain_message, settings.EMAIL_FROM,
-                        [settings.EMAIL_FEEDBACK], html_message=html_message)
+        msg = EmailMultiAlternatives(subject="{} Feedback from {}".format(settings.EMAIL_SUBJECT_PREFIX, request.user.username),
+                                     body=plain_message,
+                                     from_email=settings.EMAIL_FROM,
+                                     to=[settings.EMAIL_FEEDBACK],
+                                     reply_to=[request.user.email, settings.EMAIL_FEEDBACK])
+        msg.attach_alternative(html_message, "text/html")
+        return msg.send()
+
+
+def get_remote_ip(request):
+    if "HTTP_X_FORWARDED_FOR" in request.META:
+        ip = request.META.get("HTTP_X_FORWARDED_FOR")
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
