cff36e27bd085419434a8e70b310c1540b9456b2
Eric Wang <ezwang1@gmail.com>
2017-05-06 18:40:24 -0400

    click on table of contents header to go to top of page

---
+++ web3/static/js/guide.js
@@ -0,0 +1,27 @@
+hljs.initHighlightingOnLoad();
+$(document).ready(function() {
+    $(".guide h4, .guide h5, .guide h6").each(function() {
+        var title = $(this).text();
+        var link = title.toLowerCase().replace(/ /g, "-");
+        $(this).attr("name", link);
+        var item = $("<a />").text(title);
+        item.attr("href", "#" + link);
+        item.addClass("level");
+        item.addClass("level-" + $(this).prop("tagName").toLowerCase());
+        $(".toc").append(item);
+    });
+    $(".toc h5").click(function(e) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 400);
+    });
+    $(".toc .level").click(function(e) {
+        var loc = $(".guide [name='" + $(this).attr("href").substring(1) + "']");
+        $("html, body").animate({
+            scrollTop: loc.offset().top
+        }, 400);
+    });
+    if (window.location.hash) {
+        $("html, body").scrollTop($(".guide [name='" + window.location.hash.substring(1) + "']").offset.top);
+    }
+});
