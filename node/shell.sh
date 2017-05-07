131f4e8de0721096431dede15a412800bab7e282
Eric Wang <ezwang1@gmail.com>
2017-05-07 12:43:05 -0400

    Revert "redirect cd to site folder"
    This reverts commit 79ab506c4f0a18146de4511894bd1ca25290dfe6.

--- node/shell.sh
+++ node/shell.sh
@@ -3,14 +3,6 @@
 if [ -n "$SITE_ROOT" ];
 then
     cd $SITE_ROOT
-    function cd() {
-        new_dir="$*"
-        if [ $# -eq 0 ] || [ "$1" == "$HOME" ];
-        then
-            new_dir="$SITE_ROOT"
-        fi
-        builtin cd "${new_dir}"
-    }
 fi
 
 su - -p -s /bin/bash "$1"
