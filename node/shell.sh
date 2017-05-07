9b9d75364f211343d15569dd9c06f4a576d31138
Eric Wang <ezwang1@gmail.com>
2017-05-07 13:05:59 -0400

    shell redirects

--- node/shell.sh
+++ node/shell.sh
@@ -3,6 +3,18 @@
 if [ -n "$SITE_ROOT" ];
 then
     cd $SITE_ROOT
+
+    function cd() {
+        NEW_DIR="$*"
+        if [ $# -eq 0 ] || [ "$NEW_DIR" == "$HOME" ];
+        then
+            NEW_DIR="$SITE_ROOT"
+        fi
+        builtin cd "${NEW_DIR}"
+    }
+    export -f cd
 fi
+
+export HOME="/home/$1/"
 
 su - -p -s /bin/bash "$1"
