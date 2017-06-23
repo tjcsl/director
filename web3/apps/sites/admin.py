f2ebad3083563e6efc68690465801518eacc9744
Eric Wang <ezwang1@gmail.com>
2017-06-23 16:45:10 -0400

    register more models in admin interface

--- web3/apps/sites/admin.py
+++ web3/apps/sites/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Site
+from .models import Site, Process, Database
 # Register your models here.
 
 admin.site.register(Site)
+admin.site.register(Database)
+admin.site.register(Process)
