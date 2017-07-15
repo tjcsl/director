2f70c88cf61c1fd8c1a5bc8b9708143b73d6cf33
Eric Wang <ezwang1@gmail.com>
2017-07-15 17:08:19 -0400

    document siterequest model

--- web3/apps/request/models.py
+++ web3/apps/request/models.py
@@ -4,6 +4,25 @@ from ..users.models import User
 
 
 class SiteRequest(models.Model):
+    """Represents a site request. The request is initiated by a student, approved by a teacher, and then processed by a sysadmin.
+
+    Attributes:
+        user
+            The student that is requesting the site.
+        teacher
+            The teacher that will approve this request.
+        request_date
+            The date that the student submitted the request.
+        teacher_approval
+            Whether or not the teacher has approved the request.
+        admin_approval
+            Whether or not the administrator has processed the request.
+        activity
+            The activity that the site is intended for.
+        extra_information
+            Extra information about the request, such as additional software to be installed.
+
+    """
     user = models.ForeignKey(User, related_name="requested_sites")
     teacher = models.ForeignKey(User, related_name="site_requests")
 
