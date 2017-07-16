76e1016776795ada73ac7258af2c7753709c7a02
Eric Wang <ezwang1@gmail.com>
2017-07-16 07:34:44 -0400

    add script to renew ssl certificates

---
+++ web3/apps/sites/management/commands/renew_certs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+from web3.apps.sites.models import Domain
+from web3.apps.sites.helpers import reload_services, generate_ssl_certificate
+
+
+class Command(BaseCommand):
+    help = "Renew all LetsEncrypt certificates"
+
+    def handle(self, **options):
+        for domain in Domain.objects.all():
+            if domain.has_cert:
+                generate_ssl_certificate(domain.domain)
+                self.stdout.write("Finished {}".format(domain.domain))
+        reload_services()
