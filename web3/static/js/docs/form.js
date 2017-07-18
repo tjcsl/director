9090aa026371a0456f3e580f96ac6f71eced6598
Naitian Zhou <nzhou2011@gmail.com>
2017-07-18 10:47:14 -0400

    Docs Front End

---
+++ web3/static/js/docs/form.js
@@ -0,0 +1,22 @@
+/* global
+        ace
+*/
+$(document).ready(function () {
+    $("select").selectize();
+    var md = window.markdownit({
+        html: true,
+        linkify: true,
+        typographer: true
+    });
+
+    var editor = ace.edit("editor");
+    var textarea = $("textarea[name='content']");
+    var output = $(".output");
+
+    editor.getSession().setUseWrapMode(true);
+    editor.getSession().setMode("ace/mode/markdown");
+    editor.getSession().on("change", function () {
+        textarea.val(editor.getSession().getValue());
+        output.html(md.render(editor.getSession().getValue()));
+    });
+});
