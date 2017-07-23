2c1e3ba029da1464986ce1405ad333aeb8910a5a
Naitian Zhou <nzhou2011@gmail.com>
2017-07-23 19:59:21 -0400

    Improve Markdown and Styling

--- web3/apps/docs/models.py
+++ web3/apps/docs/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 from django.template.defaultfilters import slugify
 from simple_history.models import HistoricalRecords
@@ -50,10 +52,13 @@ class Article(models.Model, ModelDiffMixin):
 
     @property
     def html(self):
+        link_patterns = [(re.compile(r'((([A-Za-z]{2,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'), r'\1')]
         return markdown2.markdown(self.content, extras=[
             'fenced-code-blocks',
-            'header-ids'
-        ])
+            'header-ids',
+            'tables',
+            'code-friendly'
+        ], link_patterns=link_patterns)
 
     def save(self, history=False, *args, **kwargs):
         if not self.id:
