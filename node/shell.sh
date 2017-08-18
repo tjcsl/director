de9ea11373697000b36b4c6f8e30afe9b749f4d4
Eric Wang <ezwang1@gmail.com>
2017-08-18 17:30:53 -0400

    fix order

--- node/shell.sh
+++ node/shell.sh
@@ -4,7 +4,7 @@ export S="$SITE_ROOT"
 export HOME="/home/$1"
 
 if [ -z "$2" ]; then
-    su - -p -s /bin/bash "$1" -c "$2"
-else
     su - -p -s /bin/bash "$1"
+else
+    su - -p -s /bin/bash "$1" -c "$2"
 fi
