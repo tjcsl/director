c22e5342b6238440b1f5a50adca4e5620f97d771
Eric Wang <ezwang1@gmail.com>
2017-08-20 11:53:31 -0400

    make fix permissions separate function

--- web3/apps/sites/views/actions.py
+++ web3/apps/sites/views/actions.py
@@ -30,6 +30,12 @@ def config_view(request, site_id):
     return redirect("info_site", site_id=site_id)
 
 
+def fix_permissions_threaded(site):
+    t = threading.Thread(target=fix_permissions, args=(site,))
+    t.setDaemon(True)
+    t.start()
+
+
 @login_required
 def permission_view(request, site_id):
     site = get_object_or_404(Site, id=site_id)
@@ -38,9 +44,7 @@ def permission_view(request, site_id):
 
     make_site_dirs(site)
 
-    t = threading.Thread(target=fix_permissions, args=(site,))
-    t.setDaemon(True)
-    t.start()
+    fix_permissions_threaded(site)
 
     messages.success(request, "File permissions regenerated!")
     return redirect("info_site", site_id=site.id)
