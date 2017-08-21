64a761edd5029e5bc46401728984b8c38765c6e4
Eric Wang <ezwang1@gmail.com>
2017-08-21 10:06:42 -0400

    add missing import

--- web3/apps/vms/admin.py
+++ web3/apps/vms/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import VirtualMachine
+from .models import VirtualMachine, VirtualMachineHost
 # Register your models here.
 
 admin.site.register(VirtualMachine)
