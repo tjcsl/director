d948fd369a75ae8a9df71c211b0d8d83fa3a0763
Theo Ouzhinski <touzhinski@gmail.com>
2019-02-10 15:40:09 -0500

    Require login for feedback

--- web3/apps/feedback/views.py
+++ web3/apps/feedback/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from ...utils.emails import send_feedback_email
 from .models import Feedback
 
 
+@login_required
 def feedback_view(request):
     if request.method == "POST":
         comments = request.POST.get("feedback", None)
