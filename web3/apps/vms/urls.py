fb5b4a39e844989d55a18c6aebb41d99f29785e4
Theo Ouzhinski <touzhinski@gmail.com>
2019-02-10 15:54:21 -0500

    Fix build: use raw strings

--- web3/apps/vms/urls.py
+++ web3/apps/vms/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("^$", views.list_view, name="vm_list"),
-    url("^create$", views.create_view, name="create_vm"),
-    url("^(?P<vm_id>\d+)$", views.info_view, name="vm_info"),
-    url("^(?P<vm_id>\d+)/edit$", views.edit_view, name="vm_edit"),
-    url("^(?P<vm_id>\d+)/terminal$", views.terminal_view, name="vm_terminal"),
-    url("^(?P<vm_id>\d+)/delete$", views.delete_view, name="vm_delete"),
-    url("^(?P<vm_id>\d+)/start$", views.start_view, name="start_vm"),
-    url("^(?P<vm_id>\d+)/stop$", views.stop_view, name="stop_vm"),
-    url("^(?P<vm_id>\d+)/password", views.password_view, name="set_vm_password")
+    url(r"^$", views.list_view, name="vm_list"),
+    url(r"^create$", views.create_view, name="create_vm"),
+    url(r"^(?P<vm_id>\d+)$", views.info_view, name="vm_info"),
+    url(r"^(?P<vm_id>\d+)/edit$", views.edit_view, name="vm_edit"),
+    url(r"^(?P<vm_id>\d+)/terminal$", views.terminal_view, name="vm_terminal"),
+    url(r"^(?P<vm_id>\d+)/delete$", views.delete_view, name="vm_delete"),
+    url(r"^(?P<vm_id>\d+)/start$", views.start_view, name="start_vm"),
+    url(r"^(?P<vm_id>\d+)/stop$", views.stop_view, name="stop_vm"),
+    url(r"^(?P<vm_id>\d+)/password", views.password_view, name="set_vm_password")
 ]
