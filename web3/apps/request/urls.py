3b127af586ccfeb785a16ef432af8ce52c08a7e4
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-06-23 18:47:07 -0400

    Use r-strings for URL regexes

--- web3/apps/request/urls.py
+++ web3/apps/request/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("^$", views.request_view, name="request_site"),
-    url("^approve$", views.approve_view, name="approve_site"),
-    url("^admin$", views.approve_admin_view, name="admin_site")
+    url(r"^$", views.request_view, name="request_site"),
+    url(r"^approve$", views.approve_view, name="approve_site"),
+    url(r"^admin$", views.approve_admin_view, name="admin_site")
 ]
